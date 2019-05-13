dda5d450e9f35e5754bee3043050142f0ac82af9
Gábor <gabor.n.takacs@gmail.com>
2019-05-13 21:57:39 +0200

    Prototype fix for persistency related issues

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -1,14 +1,16 @@
 package se.kry.codetest;
 
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class BackgroundPoller {
 
-  public Future<List<String>> pollServices(Map<String, String> services) {
+  public Future<List<String>> pollServices(HashMap<String, JsonObject> services) {
     //TODO
     return Future.failedFuture("TODO");
   }
--- src/main/java/se/kry/codetest/MainVerticle.java
+++ src/main/java/se/kry/codetest/MainVerticle.java
@@ -4,27 +4,40 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private HashMap<String, String> services = new HashMap<>();
-  //TODO use this
+  private HashMap<String, JsonObject> services = new HashMap<>();
   private DBConnector connector;
   private BackgroundPoller poller = new BackgroundPoller();
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
   @Override
   public void start(Future<Void> startFuture) {
     connector = new DBConnector(vertx);
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
-    services.put("https://www.kry.se", "UNKNOWN");
+    connector.query("SELECT * FROM service;").setHandler(asyncResult -> {
+      if (asyncResult.succeeded()) {
+        asyncResult.result().getRows().forEach(row -> services.put(row.getString("url"), row.put("status", "UNKNOWN")));
+        logger.info("Services from DB:" + services.keySet());
+      } else {
+        logger.error("DB connection issue", asyncResult.cause());
+      }
+    });
     vertx.setPeriodic(1000 * 60, timerId -> poller.pollServices(services));
     setRoutes(router);
     vertx
@@ -32,7 +45,7 @@ public class MainVerticle extends AbstractVerticle {
         .requestHandler(router)
         .listen(8080, result -> {
           if (result.succeeded()) {
-            System.out.println("KRY code test service started");
+            logger.info("KRY code test service started");
             startFuture.complete();
           } else {
             startFuture.fail(result.cause());
@@ -40,28 +53,74 @@ public class MainVerticle extends AbstractVerticle {
         });
   }
 
-  private void setRoutes(Router router){
-    router.route("/*").handler(StaticHandler.create());
+  private void setRoutes(Router router) {
+    setStaticRoute(router);
+    setListServices(router);
+    setPostService(router);
+    setDeleteService(router);
+  }
+
+  private void setDeleteService(Router router) {
+    router.delete("/service/:service").handler(req -> {
+      try {
+        String service = URLDecoder.decode(req.pathParam("service"), "UTF-8");
+        logger.info("Deleting: " + service);
+        services.remove(service);
+        connector = new DBConnector(vertx);
+        connector.query("DELETE FROM service WHERE url=?", new JsonArray().add(service)).setHandler(asyncResult -> {
+          if (asyncResult.succeeded()) {
+            req.response()
+                .putHeader("content-type", "text/plain")
+                .end("OK");
+          } else {
+            logger.error("Failed to delete", asyncResult.cause());
+            req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Eek");
+          }
+        });
+      } catch (UnsupportedEncodingException e) {
+        req.response()
+            .putHeader("content-type", "text/plain")
+            .end("Eek");
+      }
+    });
+  }
+
+  private void setPostService(Router router) {
+    router.post("/service").handler(req -> {
+      JsonObject jsonBody = req.getBodyAsJson();
+      String url = jsonBody.getString("url");
+      String name = jsonBody.getString("name");
+      Instant createdAt = Instant.now();
+      connector = new DBConnector(vertx);
+      connector.query("INSERT INTO service (url, name, createdAt) values (?, ?, ?)", new JsonArray().add(url).add(name).add(createdAt)).setHandler(asyncResult -> {
+        if (asyncResult.succeeded()) {
+          services.put(url, jsonBody.put("status", "UNKNOWN").put("createdAt", createdAt));
+          req.response()
+              .putHeader("content-type", "text/plain")
+              .end("OK");
+        } else {
+          logger.error("post failed", asyncResult.cause());
+          req.response()
+              .putHeader("content-type", "text/plain")
+              .end("Eek");
+        }
+      });
+    });
+  }
+
+  private void setListServices(Router router) {
     router.get("/service").handler(req -> {
-      List<JsonObject> jsonServices = services
-          .entrySet()
-          .stream()
-          .map(service ->
-              new JsonObject()
-                  .put("name", service.getKey())
-                  .put("status", service.getValue()))
-          .collect(Collectors.toList());
+      List<JsonObject> jsonServices = new ArrayList<>(services.values());
       req.response()
           .putHeader("content-type", "application/json")
           .end(new JsonArray(jsonServices).encode());
     });
-    router.post("/service").handler(req -> {
-      JsonObject jsonBody = req.getBodyAsJson();
-      services.put(jsonBody.getString("url"), "UNKNOWN");
-      req.response()
-          .putHeader("content-type", "text/plain")
-          .end("OK");
-    });
+  }
+
+  private void setStaticRoute(Router router) {
+    router.route("/*").handler(StaticHandler.create());
   }
 
 }
